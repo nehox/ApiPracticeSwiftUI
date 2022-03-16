3dd83b3d2e2602d74ef6dbeb8881d52fa47142a1
Julien FICERAI <julien.ficerai@boursorama.fr>
2022-03-16 15:54:26 +0100

    Initial Commit

---
+++ ApiPracticeswiftUI/ApiPracticeswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiPracticeswiftUIApp.swift
+//  ApiPracticeswiftUI
+//
+//  Created by Julien Ficerai on 16/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ApiPracticeswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
